d2b8ee906b4ca01ab6156afb6af782bf9f69a958
Hoang-Van-Thai <hoangvanthai763@gmail.com>
2024-04-25 08:46:44 +0700

    a

--- app.py
+++ app.py
@@ -4,17 +4,12 @@ def main():
     st.markdown(
         """
         <style>
-            /* User Chat Message */
             .st-emotion-cache-janbn0 {
                 background-color: #2b313e;
             }
-
-            /* AI Chat Message */
             .st-emotion-cache-4oy321 {
                 background-color: #475063;
             }
-
-            /* Sidebar width adjustment */
             section[data-testid="stSidebar"] {
                 width: 380px !important;
             }
