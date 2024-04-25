532631dc00be28ba187f41092f487fbfc4194b7b
Hoang-Van-Thai <hoangvanthai763@gmail.com>
2024-04-25 08:42:57 +0700

    a

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+def main():
+    st.title("chatbot")
+    st.markdown(
+        """
+        <style>
+            /* User Chat Message */
+            .st-emotion-cache-janbn0 {
+                background-color: #2b313e;
+            }
+
+            /* AI Chat Message */
+            .st-emotion-cache-4oy321 {
+                background-color: #475063;
+            }
+
+            /* Sidebar width adjustment */
+            section[data-testid="stSidebar"] {
+                width: 380px !important;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+    st.sidebar.title("Chat Sessions")
+    pdf_toggle_col, voice_rec_col = st.sidebar.columns(2)
+    pdf_chat = pdf_toggle_col.checkbox("PDF Chat", key="pdf_chat", value=False)
+    st.session_key = "new_session"
+    uploaded_audio = st.sidebar.file_uploader("Upload an audio file", type=["wav", "mp3", "ogg"])
+    uploaded_image = st.sidebar.file_uploader("Upload an image file", type=["jpg", "jpeg", "png"])
+    uploaded_pdf = st.sidebar.file_uploader("Upload a pdf file", accept_multiple_files=True)
+    user_input = st.chat_input("Type your message here", key="user_input")
+if __name__ == "__main__":
+    main()
